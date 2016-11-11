c9321812040b2f09baa949e260689a797cfbae88
rbtkoz <kozovski.a@gmail.com>
2016-11-11 17:43:18 -0500

    styling

--- src/app/views/Name/Name.js
+++ src/app/views/Name/Name.js
@@ -2,8 +2,8 @@ import React from 'react';
 import nameCSS from '../Name/name.css';
 
 const Name = () => (
-  <div className ={nameCSS.myname}>flugchen x Flightline
-    <div className= {nameCSS.desc}>I am a chatbot that helps you make changes to your airberlin flight.</div>
+  <div className ={nameCSS.myname}>flugchen for airberlin
+    <div className= {nameCSS.desc}>I am a chatbot that helps you make changes to your flight.</div>
 
   </div>
 );
